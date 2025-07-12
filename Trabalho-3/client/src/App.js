9a14b46a47929343346e0cbb046d3a78498639fc
Sofia Braga <sofiamsbraga@gmail.com>
2025-07-12 10:46:55 -0300

    startando primeira pagina

--- Trabalho-3/client/src/App.js
+++ Trabalho-3/client/src/App.js
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import HomePage from './HomePage';
 
-function App(){
-  const [backendData, setBackendData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, []);
-
-  return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
-    </div>
-  )
+function App() {
+  return <HomePage />;
 }
 
-export default App
+export default App;
